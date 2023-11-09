c2a531732a59cf7ba2ca9027f51595369cb0cf0d
mansa-97 <S567011@nwmissouri.edu>
2023-11-08 22:15:58 -0600

    POST method

--- controllers/Keepsakes.js
+++ controllers/Keepsakes.js
@@ -17,9 +17,26 @@ exports.keepSake_detail = function(req, res) {
 res.send('NOT IMPLEMENTED: Costume detail: ' + req.params.id);
 };
 // Handle Costume create on POST.
-exports.keepSake_create_post = function(req, res) {
-res.send('NOT IMPLEMENTED: Costume create POST');
-};
+exports.keepSake_create_post = async function(req, res) {
+    console.log(req.body)
+    let document = new keepSake();
+    // We are looking for a body, since POST does not have query parameters.
+    // Even though bodies can be in many different formats, we will be picky
+    // and require that it be a json object
+    // {"costume_type":"goat", "cost":12, "size":"large"}
+    document.keep_model = req.body.keep_model;
+    document.keep_cost = req.body.keep_cost;
+    document.keep_quantity = req.body.keep_quantity;
+    try{
+    let result = await document.save();
+    res.send(result);
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
+    };
+    
 // Handle Costume delete form on DELETE.
 exports.keepSake_delete = function(req, res) {
 res.send('NOT IMPLEMENTED: Costume delete DELETE ' + req.params.id);
