22d3edad5a79db12ddd805be18e00265189c6599
mansa-97 <S567011@nwmissouri.edu>
2023-11-14 20:40:06 -0600

    Resource for delete

--- controllers/Keepsakes.js
+++ controllers/Keepsakes.js
@@ -84,3 +84,16 @@ exports.keepSake_detail = async function(req, res) {
     res.send(`{"error": document for id ${req.params.id} not found}`);
     }
     };
+// Handle Costume delete on DELETE.
+exports.keepSake_delete = async function(req, res) {
+    console.log("delete " + req.params.id)
+    try {
+    result = await Keepsakes.findByIdAndDelete( req.params.id)
+    console.log("Removed " + result)
+    res.send(result)
+    } catch (err) {
+    res.status(500)
+    res.send(`{"error": Error deleting ${err}}`);
+    }
+    };
+    
