18308a544b96c0383125368660795561e8754704
mansa-97 <S567011@nwmissouri.edu>
2023-11-16 16:16:33 -0600

    passport code

--- app.js
+++ app.js
@@ -2,8 +2,30 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
+var cookieParser = require('cookie-parser');  
 var logger = require('morgan');
+
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    Account.findOne({ username: username })
+      .then(function(user) {
+        if (err) { return done(err); }
+        if (!user) {
+          return done(null, false, { message: 'Incorrect username.' });
+        }
+        if (!user.validPassword(password)) {
+          return done(null, false, { message: 'Incorrect password.' });
+        }
+        return done(null, user);
+      })
+      .catch(function(err) {
+        return done(err);
+      });
+    })
+)
+
 var keepSake = require("./models/Keepsakes");
 
 require('dotenv').config();
@@ -55,7 +77,9 @@ var resourceRouter = require('./routes/resource');
 
 
 var app = express();
-
+app.get('/board', (req, res) => {
+  res.render('board');
+});
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -64,6 +88,15 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
@@ -73,10 +106,13 @@ app.use('/',boardRouter);
 app.use('/choose',chooseRouter);
 app.use('/resource',resourceRouter);
 
-
-app.get('/board', (req, res) => {
-  res.render('board');
-});
+// passport config
+// Use the existing connection
+// The Account model
+var Account =require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
