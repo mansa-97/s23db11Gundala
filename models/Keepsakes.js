f64b9c4c2c31be0ea7a8a6d31ced4fcea0e60196
mansa-97 <S567011@nwmissouri.edu>
2023-11-28 19:53:59 -0600

    validation failure

--- models/Keepsakes.js
+++ models/Keepsakes.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 const keepSakeSchema = mongoose.Schema({
     keep_model:String,
-    keep_cost:Number,
+    keep_cost: {type:Number,min:10,max:5000},
     keep_quantity:Number
 })
 module.exports = mongoose.model("KeepSakes",
